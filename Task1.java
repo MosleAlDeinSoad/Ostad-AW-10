91138448329212b3a837c90af445da44464c91cf
Mosle Al Dein Soad <soad2015prince@gmail.com>
2024-06-30 23:49:49 +0600

    Assignment02

---
+++ Task1.java
@@ -0,0 +1,83 @@
+// Base class
+class BankAccount {
+    protected double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+// Subclass: SavingsAccount
+class SavingsAccount extends BankAccount {
+    private static final double WITHDRAWAL_LIMIT = 1000;
+
+    public SavingsAccount(double balance) {
+        super(balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount > WITHDRAWAL_LIMIT) {
+            System.out.println("Withdrawal amount exceeds the limit of " + WITHDRAWAL_LIMIT);
+        } else {
+            super.withdraw(amount);
+        }
+    }
+}
+
+// Subclass: CheckingAccount
+class CheckingAccount extends BankAccount {
+    private static final double TRANSACTION_FEE = 2.5;
+
+    public CheckingAccount(double balance) {
+        super(balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        double totalAmount = amount + TRANSACTION_FEE;
+        if (totalAmount > balance) {
+            System.out.println("Insufficient funds for withdrawal and transaction fee.");
+        } else {
+            super.withdraw(totalAmount);
+            System.out.println("Transaction fee of " + TRANSACTION_FEE + " applied.");
+        }
+    }
+}
+
+// Main class to test the implementation
+public class Task1 {
+    public static void main(String[] args) {
+        BankAccount savings = new SavingsAccount(1500);
+        BankAccount checking = new CheckingAccount(2000);
+
+        savings.deposit(500);
+        savings.withdraw(1200);
+        savings.withdraw(500);
+
+        checking.deposit(500);
+        checking.withdraw(2000);
+        checking.withdraw(500);
+
+        System.out.println("Savings Account Balance: " + savings.getBalance());
+        System.out.println("Checking Account Balance: " + checking.getBalance());
+    }
+}
---
+++ Task2.java
@@ -0,0 +1,82 @@
+// Base class
+abstract class Shape {
+    public abstract void draw();
+    public abstract double calculateArea();
+}
+
+// Subclass: Circle
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Subclass: Square
+class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Square");
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+}
+
+// Subclass: Triangle
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Triangle");
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+// Main class to test the implementation
+public class Task2 {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape square = new Square(4);
+        Shape triangle = new Triangle(3, 6);
+
+        circle.draw();
+        System.out.println("Area of Circle: " + circle.calculateArea());
+
+        square.draw();
+        System.out.println("Area of Square: " + square.calculateArea());
+
+        triangle.draw();
+        System.out.println("Area of Triangle: " + triangle.calculateArea());
+    }
+}
